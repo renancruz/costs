9e00d993f758a3080bf1b3b3ea1c4ae42c6abade
Renan Cruz <renan.frcr@gmail.com>
2022-10-29 09:51:25 -0300

    Criando a página NewProject

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,12 @@
-export default function NewProject () {
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
+export default function NewProject() {
   return (
-    <h1>NewProject</h1>
+    <div className={styles.newproject_container}>
+      <h1>Cria Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm/>
+    </div>
   )
 }
